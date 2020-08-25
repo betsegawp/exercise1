f76d055ef82ba03284da65a09958d07f3f916bb7
betsegawp <betsegaw.tereda@gmail.com>
2020-08-24 21:33:20 -0400

    whatever

---
+++ src/Trial.java
@@ -0,0 +1,13 @@
+public class Trial {
+    public static void main(String[] args){
+        int i=50;
+        double p=24.99;
+        String userName="Bobby Johnson";
+        System.out.println("Test score initialized is: "+i);
+        System.out.println("price tag: "+p);
+        System.out.println("userName: " +userName);
+
+
+    }
+
+}
